3ea6e85a112e42a19d385c890ce7399bf1cf60bb
Nandita Venkat <nanditavenkat@gmail.com>
2020-04-12 22:21:02 +0000

    Done.

--- spec/cartoon_collections_spec.rb
+++ spec/cartoon_collections_spec.rb
@@ -7,7 +7,7 @@ describe "Using Each" do
 
       output = capture_stdout do
         greet_characters(dwarves)
-      end
+       end
 
       # This regex allows for arbitrary characters between
       # the numbering and the name
